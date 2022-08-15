be08403d2cd4d30422b77ce7d760bd90b3ae220f
Al Biruni <birunimamak@gmail.com>
2022-08-15 17:34:32 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const nodeHtmlToImage = require("node-html-to-image");
+const bodyParser = require("body-parser");
+
+const axios = require("axios");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 4400;
+
+const dataImagePrefix = `data:image/png;base64,`;
+
+/*
+
+method: POST
+route: /img2html
+body: {
+    "certificate_bg": "url",
+    "certificate_values": {
+        "name": "Muhammad Al Biruni",
+        "status": "Peserta"
+    }
+}
+
+*/
+app.post("/html2img", async (req, res) => {
+  const image = await nodeHtmlToImage({
+    html: req.body.html_code,
+    content: req.body.content || {},
+  });
+  const finalImage = `${dataImagePrefix}${image.toString("base64")}`;
+
+  res.json({ result: finalImage });
+});
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`App is listening to PORT: http://localhost:${PORT}`);
+});
