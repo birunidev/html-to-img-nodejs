fc2ac2d7ac48c0de98a1202338e7eadf8bd24242
Al Biruni <birunimamak@gmail.com>
2022-08-15 17:42:52 +0700

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const nodeHtmlToImage = require("node-html-to-image");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const axios = require("axios");
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
